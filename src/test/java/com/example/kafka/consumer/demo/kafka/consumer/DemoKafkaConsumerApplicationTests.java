4fd284a3424027603d86028dc21f3cc087a5fa5c
Ratkovski <drezcry@hotmail.com>
2022-02-06 21:55:12 -0300

    Codebase

---
+++ src/test/java/com/example/kafka/consumer/demo/kafka/consumer/DemoKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka.consumer.demo.kafka.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
